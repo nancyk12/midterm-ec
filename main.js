6c4477ed8b4d7daa4472755b2dabfd41c5099c9a
nancyk12 <nancyk12@hotmail.com>
2022-11-03 11:18:52 -0400

    update files'

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
+/*sfunction twoSmallest(array){
+
+}
 
 
  if (typeof twoSmallest === 'undefined') {
@@ -9,3 +12,29 @@
  module.exports = {
     twoSmallest,
   }
+*/
+
+  //let arr = [ 6, 3, 5, 2, 9 ];
+  function twoSmallest(arr){
+		let smallest = arr[0]; 
+		let secondSmallest = arr[1]; 
+
+		for(var i = 0; i < arr.length; i++) { 
+			if(arr[i] < smallest) {  
+				smallest = arr[i];  
+        //console.log(smallest);
+			}  
+      if (arr.length < 2){
+        return undefined;
+      }
+			if(arr[i] > smallest && arr[i] < secondSmallest ) { 
+				secondSmallest = arr[i]; 
+        //console.log(secondSmallest);
+			} 
+		} let newArray = [smallest, secondSmallest];
+    return newArray;
+  }
+		console.log(twoSmallest([10]));
+
+    
+
